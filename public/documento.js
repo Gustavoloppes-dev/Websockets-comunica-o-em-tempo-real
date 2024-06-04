ba3c022e4894990c0e639f277930e1899ff6313e
Gustavoloppes-dev <gustavoloppes.dev@gmail.com>
2024-06-04 11:41:18 -0300

    Agrupando clientes em salas do Socket.Io, emitindo eventos para salas específicas e enviando dados para clientes com reconhecimento(ou Acknowledgment)

--- public/documento.js
+++ public/documento.js
@@ -1,14 +1,33 @@
-import { emitirTextoEditor } from "./socket-front-documento.js";
+import {
+  emitirTextoEditor,
+  selecionarDocumento,
+} from "./socket-front-documento.js";
+
+const parametros = new URLSearchParams(window.location.search);
+const nomeDocumento = parametros.get("nome");
 
 const textoEditor = document.getElementById("editor-texto");
+const tituloDocumento = document.getElementById("titulo-documento");
+
+tituloDocumento.textContent = nomeDocumento || "Documento sem titulo";
+
+selecionarDocumento(nomeDocumento);
+
+// textoEditor.addEventListener("keyup", (e) => {
+//     // console.log(textoEditor.value);
+//     emitirTextoEditor(textoEditor.value, nomeDocumento);
+// })
 
 textoEditor.addEventListener("keyup", (e) => {
-    // console.log(textoEditor.value);
-    emitirTextoEditor(textoEditor.value)
-})
+  // console.log(textoEditor.value);
+  emitirTextoEditor({
+    texto: textoEditor.value,
+    nomeDocumento,
+  });
+});
 
 function atualizaTextoEditor(texto) {
-    textoEditor.value = texto;
+  textoEditor.value = texto;
 }
 
 export { atualizaTextoEditor };
